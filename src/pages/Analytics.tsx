cd99dd414a6f765238f1f71fd8c5d7c67704b7b1
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-06-09 09:46:10 -0500

    feat: display analytics data in analytics page

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,8 +1,53 @@
+import { usePostAnalytics } from "../hooks/usePostAnalytics";
+import { useAppContext } from "../context/appContext";
+import { Post } from "../lib/definitions";
 import Navbar from "../components/Navbar";
+
 const Analytics = () => {
+
+  const { posts, isLoading } = useAppContext();
+  const [analytics] = usePostAnalytics();
+  const computedPosts = posts.map((post: Post) => {
+      return { ...post, numberOfClicks: analytics[post.id] ?? 0 };
+  });
+
+  if (isLoading) {
+      return <div>Loading...</div>;
+  }
     return (
         <>
             <Navbar />
+            <main className='container'>
+                <div className='analytics'>
+                    <h1 className='analytics__heading'>Post Analytics</h1>
+                    <table className='analytics__table'>
+                        <thead>
+                            <tr>
+                                <th>S/N</th>
+                                <th>Card ID</th>
+                                <th>Card Title</th>
+                                <th>Number of Clicks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {computedPosts.map((post, index) => {
+                                const { id, title, numberOfClicks } = post;
+                                const serialNumber = index + 1;
+                                return (
+                                    <tr key={id}>
+                                        <td>{serialNumber}</td>
+                                        <td>{id}</td>
+                                        <td className='analytics__post-title'>
+                                            {title}
+                                        </td>
+                                        <td>{numberOfClicks}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
         </>
     );
 };
